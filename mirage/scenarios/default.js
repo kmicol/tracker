94e25a547f5c4abfce06a64b3e6c85fe6c6163f8
Justin Brown <justinbrown.work@gmail.com>
2016-08-08 14:57:35 -0400

    Use the default scenario instead of returning data in the Mirage config.

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,4 +1,4 @@
-export default function(/* server */) {
+export default function(server) {
 
   /*
     Seed your development database using your factories.
@@ -8,4 +8,19 @@ export default function(/* server */) {
   */
 
   // server.createList('post', 10);
+  server.create('post', {
+    title: 'Oh my shit a Tractor',
+    subtitle: 'He cannot believe there is a tractor. Can you?',
+    author: 'Patrick Smatlock'
+  });
+  server.create('post', {
+    title: 'The government made me fat, now they need to fix it.',
+    subtitle: 'This overweight woman blames the government for her unhealthy eating habbits. Whos to blame?',
+    author: 'Ms. Messer'
+  });
+  server.create('post', {
+    title: 'This is the one trick Doctors dont want you to know about!',
+    subtitle: 'This is a Clickbait link, go ahead and click!',
+    author: 'Doctor steve Brule'
+  });
 }
