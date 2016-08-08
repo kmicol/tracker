73efbc6ea0c13bc100ca378347cff6a2f25afab5
Justin Brown <justinbrown.work@gmail.com>
2016-08-08 18:34:53 -0400

    Fix syntax error. Return statement needs to be inside the model function.

--- app/routes/posts/show.js
+++ app/routes/posts/show.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
-	return this.store.find('post', params.post_id);
-
+  model(params) {
+    return this.store.find('post', params.post_id);
+  }
 });
