6f77c3413bb8765671a8b306bb6d42df8c9b5aa8
Kevin Micol <kevin.d.micol@gmail.com>
2016-08-08 16:29:28 -0400

    created a form for making posts in posts/new template, and added createRecords('post') model

--- app/models/post.js
+++ app/models/post.js
@@ -5,4 +5,13 @@ export default Model.extend({
   title: attr('string'),
   subtitle: attr('string'),
   author: attr('string'),
+  model() {
+    return this.store.createRecord('post');
+  },
 });
+
+
+
+
+
+
